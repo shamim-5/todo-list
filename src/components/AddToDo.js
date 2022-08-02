9c7931bbc835c0ba7eb789c51774952c57330b53
mss38 <mss38k@gmail.com>
2022-08-02 22:48:14 +0600

    added functionality done

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,11 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddToDo = () => {
+  const navigate = useNavigate();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const addedToDo = {
+      heading: e.target.heading.value,
+      description: e.target.description.value,
+      completed: e.target.completed.value,
+      comments: e.target.comments.value,
+    };
+    fetch(`http://localhost:5000/add`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(addedToDo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const handleEscButton = () => {
+    navigate("/");
+  };
   return (
     <div className="">
-      <h2>Add</h2>
+      <h2 className="text-center uppercase text-4xl font-semibold text-[#38BDF8]">Add todo's</h2>
+      <div className="border border-gray-700 bg-slate-300 p-6 rounded-lg shadow-lg mt-12 md:w-1/2 mx-auto text-slate-900">
+        <form onSubmit={handleSubmit}>
+          <label className="flex items-end justify-start">
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 text-sm font-medium text-slate-700 w-48 md:w-52">
+              Heading
+            </span>
+            <input
+              type="text"
+              name="heading"
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1 w-full "
+              placeholder="To-Do Heading"
+              required
+            />
+          </label>
+          <label className="flex items-end justify-start">
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 text-sm font-medium text-slate-700 w-48 md:w-52">
+              Description
+            </span>
+            <input
+              type="text"
+              name="description"
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1 w-full "
+              placeholder="To-Do Description"
+              required
+            />
+          </label>
+          <label className="flex items-end justify-start">
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 text-sm font-medium text-slate-700 w-48 md:w-52">
+              Completed
+            </span>
+            <select
+              name="completed"
+              id="completed"
+              className="mt-1 px-3 py-2  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1 w-full"
+            >
+              <option value="true">true</option>
+              <option value="false">false</option>
+            </select>
+          </label>
+
+          <label className="flex items-end justify-start">
+            <span className="text-sm font-medium text-slate-700 w-48 md:w-52">Comments</span>
+            <input
+              type="text"
+              name="comments"
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1 w-full "
+              placeholder="Enter Comments"
+            />
+          </label>
+          <div className="flex justify-end py-9 md:w-4/5">
+            <button
+              onClick={handleEscButton}
+              className="text-sm px-6 py-2 uppercase  text-center rounded-lg text-red-500 mr-12 border border-red-300"
+            >
+              <span>cancel</span>
+              <span className="flex items-center">(esc)</span>
+            </button>
+            <button
+              type="submit"
+              className="text-sm bg-[#599636] px-6 py-2 uppercase  text-center font-semibold rounded-lg text-white"
+            >
+              <span>submit</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [todo, setTodo] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("http://localhost:5000").then((res) => setTodo(res.data));
   }, []);
 
+  const handleAddNowButton = () => {
+    navigate("/add");
+  };
   return (
     <div>
-      <h2 className="text-center uppercase text-4xl font-semibold text-[#38BDF8]">Available todos</h2>
+      <h2 className="text-center uppercase text-4xl font-semibold text-[#38BDF8]">Available todo's</h2>
       <div>
+        <div className="flex justify-around items-center flex-row mt-7 py-4 border border-[#1d4065] rounded">
+          <h2>Need to Add more todo's ?</h2>
+          <button onClick={handleAddNowButton} class="btn btn-secondary">
+            Add now
+          </button>
+        </div>
         <section className="grid lg:grid-cols-3 grid-cols-1 gap-4 py-12">
           {todo &&
             todo.map((item) => {
@@ -23,7 +34,8 @@ const Home = () => {
                       <h2 class="card-title">{heading}</h2>
                       <p>{description}</p>
                       <small>{comments}</small>
-                      <div class="card-actions justify-end">
+                      <div class="card-actions justify-around">
+                        <button class="btn bg-transparent text-red-700">Delete</button>
                         <button class="btn btn-primary">Mark as complete</button>
                       </div>
                     </div>
